bda686b66204ec46b64916fe9a34bb213236b2a8
astorias <4drianthkiller@gmail.com>
2024-07-31 12:28:47 -0600

    Se agrega el proyecto

---
+++ WebApiPets/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPets.Models;
+
+namespace WebApiPets.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Pets> Pets { get; set; }
+    }
+}
---
+++ WebApiPets/Models/Pets.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiPets.Models
+{
+    public class Pets
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Raza { get; set; }
+        public int Age { get; set; }
+    }
+}
